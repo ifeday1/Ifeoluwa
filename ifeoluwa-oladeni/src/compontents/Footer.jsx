7e83db5cac1e508b892f5b65379e3172a4b0335c
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2025-04-14 10:45:50 +0100

    check

--- ifeoluwa-oladeni/src/compontents/Footer.jsx
+++ ifeoluwa-oladeni/src/compontents/Footer.jsx
@@ -4,9 +4,10 @@ import { motion } from 'framer-motion';
 const Footer = () => {
   return (
     <>
-      <div className=' flex justify-center '>
+      <div className='grid justify-center'>
         <footer className=' text-white px-6 py-8 md:px-20 max-w-[1200px] '>
           <div className='max-w-8xl mx-auto'>
+            {/* Gradient Text */}
             <h2 className='text-left md:text-7xl text-5xl font-semibold'>
               <span className='bg-gradient-to-r from-white to-gray-600 bg-clip-text text-transparent block'>
                 Let's work
@@ -31,7 +32,7 @@ const Footer = () => {
 
             {/* Footer bottom section */}
             <div className=''>
-              <div className='flex flex-col md:flex-row justify-between items-start md:items-center max-w-[1200px]'>
+              <div className='flex flex-col md:flex-row justify-between items-start md:items-center'>
                 <p className='text-gray-500'>
                   @Ifeoluwa Oladeni 2025 - DESIGNED AND DEVELOPED BY ME
                 </p>
--- ifeoluwa-oladeni/src/pages/Works.jsx
+++ ifeoluwa-oladeni/src/pages/Works.jsx
@@ -10,21 +10,22 @@ import Gamerwork from '../assets/gamerwork.png';
 const Works = () => {
   return (
     <>
-      <div className=' gap-8 p-6 md:p-20  items-center'>
-        {/* Left Section */}
-        <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8 }}
-          className='flex flex-col justify-center items-start'
-        >
-          <p className='md:text-7xl text-5xl font-bold text-neutral-500 uppercase'>
-            <span className=' block'>Products</span>
-            <span className='block'>crafted</span>
-            <span className='text-white block'>with LOVE ❤️ </span>
-          </p>
+      <div className=' flex justify-center'>
+        <div className=' gap-8 p-6 md:p-0 mt-0 md:mt-15  items-center max-w-[1200px]'>
+          {/* Left Section */}
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+            className='flex flex-col justify-center items-start'
+          >
+            <p className='md:text-7xl text-5xl font-bold text-neutral-500 uppercase'>
+              <span className=' block'>Products</span>
+              <span className='block'>crafted</span>
+              <span className='text-white block'>with LOVE ❤️ </span>
+            </p>
 
-          {/* <motion.a
+            {/* <motion.a
             href='https://www.linkedin.com/in/ifeoluwa-oladeni'
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
@@ -46,220 +47,221 @@ const Works = () => {
               />
             </svg>
           </motion.a> */}
-        </motion.div>
+          </motion.div>
 
-        {/* Image Grid */}
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 pt-20'>
-          {/* Project 1 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/frontier'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Frontwork}
-                alt='Project 1'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
+          {/* Image Grid */}
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 pt-20'>
+            {/* Project 1 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/frontier'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Frontwork}
+                  alt='Project 1'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
 
-            {/* Project Title */}
-            <h3 className='text-lg font-semibold mt-3 text-white'>
-              FRONTIER SOLAR TECH
-            </h3>
+              {/* Project Title */}
+              <h3 className='text-lg font-semibold mt-3 text-white'>
+                FRONTIER SOLAR TECH
+              </h3>
 
-            {/* Smaller Horizontal Info Boxes */}
-            <div className='flex gap-2 mt-3'>
-              {['Branding', 'UI/UX', 'Web development'].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
+              {/* Smaller Horizontal Info Boxes */}
+              <div className='flex gap-2 mt-3'>
+                {['Branding', 'UI/UX', 'Web development'].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
+            </div>
+
+            {/* Project 2 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/crowdcarry'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Crowdwork}
+                  alt='Project 2'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
+
+              {/* Project Title */}
+              <h3 className='text-lg font-semibold mt-3 text-white'>
+                CROWD CARRY
+              </h3>
+
+              {/* Smaller Horizontal Info Boxes */}
+              <div className='flex gap-2 mt-3'>
+                {['UI Design'].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
 
-          {/* Project 2 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/crowdcarry'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Crowdwork}
-                alt='Project 2'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
+          {/* Image Grid */}
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:pt-15 pt-5'>
+            {/* Project 1 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/clickcart'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Clickwork}
+                  alt='Project 1'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
 
-            {/* Project Title */}
-            <h3 className='text-lg font-semibold mt-3 text-white'>
-              CROWD CARRY
-            </h3>
+              {/* Project Title */}
+              <h3 className='text-lg font-semibold mt-3 text-white'>
+                CLICK CART
+              </h3>
 
-            {/* Smaller Horizontal Info Boxes */}
-            <div className='flex gap-2 mt-3'>
-              {['UI Design'].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
+              {/* Smaller Horizontal Info Boxes */}
+              <div className='flex gap-2 mt-3'>
+                {[
+                  'Brand Identity Design',
+                  'UI /UX design',
+                  'UX Writing',
+                  ' UX Research',
+                ].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
             </div>
-          </div>
-        </div>
 
-        {/* Image Grid */}
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:pt-15 pt-5'>
-          {/* Project 1 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/clickcart'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Clickwork}
-                alt='Project 1'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
+            {/* Project 2 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/incognito'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Incogwork}
+                  alt='Project 2'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
 
-            {/* Project Title */}
-            <h3 className='text-lg font-semibold mt-3 text-white'>
-              CLICK CART
-            </h3>
+              {/* Project Title */}
+              <h3 className='text-lg font-semibold mt-3 uppercase text-white'>
+                Incognito Tech solutions
+              </h3>
 
-            {/* Smaller Horizontal Info Boxes */}
-            <div className='flex gap-2 mt-3'>
-              {[
-                'Brand Identity Design',
-                'UI /UX design',
-                'UX Writing',
-                ' UX Research',
-              ].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
-            </div>
-          </div>
-
-          {/* Project 2 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/incognito'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Incogwork}
-                alt='Project 2'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
-
-            {/* Project Title */}
-            <h3 className='text-lg font-semibold mt-3 uppercase text-white'>
-              Incognito Tech solutions
-            </h3>
-
-            {/* Smaller Horizontal Info Boxes */}
-            <div className='flex gap-2 mt-3'>
-              {['UI/UX Design'].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {/* Image Grid */}
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 pt-15'>
-          {/* Project 1 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/winnersbc'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Winnerswork}
-                alt='Project 1'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
-
-            <h3 className='text-lg font-semibold mt-3 text-white uppercase'>
-              WInners Baptist church
-            </h3>
-
-            <div className='flex gap-2 mt-3'>
-              {['UI /UX design', 'Web Development'].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
+              {/* Smaller Horizontal Info Boxes */}
+              <div className='flex gap-2 mt-3'>
+                {['UI/UX Design'].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
 
-          {/* Project 2 */}
-          <div className='flex flex-col items-start'>
-            <motion.a
-              href='/works/gamer'
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              className='block overflow-hidden rounded-lg shadow-lg w-full'
-            >
-              <img
-                src={Gamerwork}
-                alt='Project 2'
-                className='w-full object-cover rounded-lg'
-              />
-            </motion.a>
+          {/* Image Grid */}
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 pt-15'>
+            {/* Project 1 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/winnersbc'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Winnerswork}
+                  alt='Project 1'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
 
-            {/* Project Title */}
-            <h3 className='text-lg font-semibold mt-3 uppercase text-white'>
-              Gamer
-            </h3>
+              <h3 className='text-lg font-semibold mt-3 text-white uppercase'>
+                WInners Baptist church
+              </h3>
 
-            {/* Smaller Horizontal Info Boxes */}
-            <div className='flex gap-2 mt-3'>
-              {['UI/UX Design'].map((text, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05 }}
-                  className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
-                >
-                  {text}
-                </motion.div>
-              ))}
+              <div className='flex gap-2 mt-3'>
+                {['UI /UX design', 'Web Development'].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
+            </div>
+
+            {/* Project 2 */}
+            <div className='flex flex-col items-start'>
+              <motion.a
+                href='/works/gamer'
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className='block overflow-hidden rounded-lg shadow-lg w-full'
+              >
+                <img
+                  src={Gamerwork}
+                  alt='Project 2'
+                  className='w-full object-cover rounded-lg'
+                />
+              </motion.a>
+
+              {/* Project Title */}
+              <h3 className='text-lg font-semibold mt-3 uppercase text-white'>
+                Gamer
+              </h3>
+
+              {/* Smaller Horizontal Info Boxes */}
+              <div className='flex gap-2 mt-3'>
+                {['UI/UX Design'].map((text, index) => (
+                  <motion.div
+                    key={index}
+                    whileHover={{ scale: 1.05 }}
+                    className='bg-gray-800 px-4 py-2 rounded-lg text-center text-sm text-white'
+                  >
+                    {text}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
